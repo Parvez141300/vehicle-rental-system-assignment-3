505c0c8a43fdce90d546ecb05ad7306107a85b9c
Parvez141300 <parvezhossain744471@gmail.com>
2025-12-22 22:32:40 +0600

    edited the queries.sql file

--- queries.sql
+++ queries.sql
@@ -1,5 +1,4 @@
--- Query 1: JOIN
--- Retrieve booking information along with:
+-- Query 1: JOIN Retrieve booking information along with:
 -- 1) Customer name
 -- 2) Vehicle name
 select
@@ -13,6 +12,17 @@ from
   Bookings b
   inner join Users u on u.user_id = b.user_id
   inner join Vehicles v on v.vehicle_id = b.vehicle_id;
+
+solution:
+booking_id  customer_name   vehicle_name    start_date  end_date    status 
+1       	Parvez Hossain	Toyota Corolla	2025-01-01	2025-01-06	confirmed
+2       	Rahim Uddin	    Honda Civic	    2025-02-10	2025-02-13	pending
+3       	Karim Ahmed	    Yamaha R15	    2025-03-05	2025-03-12	confirmed
+4       	Nusrat Jahan	Ford F-150	    2025-04-01	2025-04-03	pending
+5       	Parvez Hossain	Yamaha R15	    2025-01-01	2025-01-06	confirmed
+6       	Parvez Hossain	Honda Civic	    2025-01-01	2025-01-06	confirmed
+7       	Nusrat Jahan	Honda Civic	    2025-04-01	2025-04-03	pending
+
 
 -- Query 2: EXISTS
 -- Find all vehicles that have never been booked.
@@ -36,6 +46,12 @@ where
       b.vehicle_id = v.vehicle_id
   );
 
+solution:
+vehicle_id  name        model       registration_number     rental_price    status
+5	        BMW	        2015	    DHA-2244	            150.00	        available
+6	        Nissan GTR	2011	    DHA-1144	            150.00	        available
+
+
 -- Query 3: WHERE
 -- Retrieve all available vehicles of a specific type (e.g. cars).
 -- Concepts used: SELECT, WHERE
@@ -47,6 +63,13 @@ where
 type
   = 'car'
   and status = 'available';
+
+solution:
+vehicle_id  name            type    model   registration_number     rental_price    status
+1	        Toyota Corolla	car	    2022	DHA-1234	            50.00	        available
+5	        BMW	            car	    2015	DHA-2244	            150.00	        available
+6	        Nissan GTR	    car	    2011	DHA-1144	            150.00	        available
+
 
 -- Query 4: GROUP BY and HAVING
 -- Find the total number of bookings for each vehicle and 
@@ -60,3 +83,7 @@ group by
   vehicle_id
 having
   count(vehicle_id) > 2;
+
+solution:
+booking_count
+3
